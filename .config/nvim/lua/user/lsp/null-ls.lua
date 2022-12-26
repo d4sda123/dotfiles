1d48bc2106afc568a5852843efed92a4bf4fe0c9
dasda <roalgenio14@gmail.com>
2022-12-25 21:31:05 -0500

    remove prettier

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -4,12 +4,12 @@ local formatting = null_ls.builtins.formatting
 local code_actions = null_ls.builtins.code_actions
 
 local sources = {
-	formatting.prettier,
-	formatting.black,
-	formatting.stylua,
-	code_actions.gitsigns,
+    -- formatting.prettier,
+    formatting.black,
+    formatting.stylua,
+    code_actions.gitsigns,
 }
 
 null_ls.setup({
-	sources = sources,
+    sources = sources,
 })
